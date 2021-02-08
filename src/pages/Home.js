c36d38e3444c2d260c2e9806f1f58d11bcbf1dec
brendalima <brenda.c.lima@poli.ufrj.br>
2021-02-07 21:41:58 -0300

    improvement: finishing home page structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,17 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Home() {
   return (
     <div>
-      <header>
-        <img src="" alt="logo" />
-        {/* <Link>Encontre uma mentora</Link>
-        <Link>Sou uma mentora</Link> */}
-      </header>
+      <Header />
       <section className="hero">
-        <h2>POTENCIALIZANDO ENCONTROS</h2>
-        <h2>TRANSFORMANDO VIDAS</h2>
+          <h2>POTENCIALIZANDO ENCONTROS</h2>
+          <h2>TRANSFORMANDO VIDAS</h2>
       </section>
       <h1>
         MENTORIA
         <strong>GRATUITA</strong>
-        {' '}
         DE TECNOLOGIA PARA MULHERES
       </h1>
       <h2>Conecte-se com especialistas para esclarecer suas dúvidas</h2>
@@ -25,7 +21,6 @@ function Home() {
             Sabemos as dificuldades que mulheres enfrentam no
             mercado de tecnologia e o quão difícil pode ser
             planejar sua carreira. Estamos aqui para te dizer:
-            {' '}
           </p>
           <h3><strong>VOCÊ NÃO ESTÁ SOZINHA!</strong></h3>
           <p>
@@ -36,6 +31,31 @@ function Home() {
         </div>
         <div>
           <img src="" alt="mulheres diversas" />
+          <button
+            onClick={() => history.push('/login')}
+          >
+            Quero agendar uma mentoria
+          </button>
+        </div>
+        <div>
+          <img src="/images/image 9.png" alt="gráfico de atributos" />
+          <button
+            onClick={() => history.push('/login')}
+          >
+            Quero ser uma mentora
+          </button>
+        </div>
+        <div>
+          <p>
+            Compartilhe seus conhecimentos com a comunidade e
+            ajude outras mulheres a crescerem.
+          </p>
+          <h3><strong>AQUI VOCÊ DESENVOLVE SUA LIDERANÇA</strong></h3>
+          <p>
+            Essa também é uma oportunidade para você desenvolver 
+            suas habilidades comportamentais e dar um passo 
+            em sua carreira.
+          </p>
         </div>
       </div>
     </div>
